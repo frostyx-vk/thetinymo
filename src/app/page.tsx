8af4a12c25df3325be3cf62f28872f780b866be8
viktor <vk.xam@yandex.ru>
2025-10-05 12:50:45 +0700

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
+          efefefef
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
